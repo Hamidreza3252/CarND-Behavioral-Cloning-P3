f4703a23816cf5b3e4cd629eb836e1e2416e74a5
Hamidreza3252 <Hamid.Mirkhani@gmail.com>
2019-07-15 08:34:14 -0400

    code transfer-01

---
+++ dataProcessingModules.py
@@ -0,0 +1,99 @@
+import keras
+import numpy as np
+
+
+class DataGenerator(keras.utils.Sequence):
+    def __init__(self, list_ids, labels, batch_size=32, dim=(32, 32, 3), n_channels=1, n_classes=10, shuffle=True):
+        """
+
+        Initialization:
+
+        :param list_ids:
+        :param labels:
+        :param batch_size:
+        :param dim:
+        :param n_channels:
+        :param n_classes:
+        :param shuffle:
+        """
+
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_ids = list_ids
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+        self.indexes = None
+        self.shuffle = True
+
+    def __len__(self):
+
+        """
+        Denotes the number of batches per epoch
+
+        :return:
+        """
+
+        return int(np.floor(len(self.list_ids) / self.batch_size))
+
+    def __getitem__(self, index):
+        """
+
+        Generate one batch of data
+
+        :param index:
+        :return:
+        """
+
+        # Generate indexes of the batch
+        from_index = index*self.batch_size
+        indexes = self.indexes[from_index:from_index + self.batch_size]
+        # indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+
+        # Find list of IDs
+        list_ids_temp = [self.list_ids[k] for k in indexes]
+
+        # Generate data
+        x_data, y_data = self.__data_generation(list_ids_temp)
+
+        return x_data, y_data
+
+    def on_epoch_end(self):
+        """
+
+        Updates indexes after each epoch
+
+        :return:
+        """
+
+        self.indexes = np.arange(len(self.list_ids))
+
+        if (self.shuffle == True):
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_ids_temp):
+        """
+
+        Generates data containing batch_size samples # x_data : (n_samples, *dim, n_channels)
+
+        :param list_ids_temp:
+        :return:
+        """
+
+        # Initialization
+        x_data = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y_data = np.empty((self.batch_size), dtype=int)
+
+        # Generate data
+        for i, id in enumerate(list_ids_temp):
+            # Store sample
+            x_data[i, ] = np.load("data/" + id + ".npy")
+
+            # Store class
+            y_data[i] = self.labels[id]
+
+        return x_data, keras.utils.to_categorical(y_data, num_classes=self.n_classes)
+
--- selfDrivingCarModules.py
+++ selfDrivingCarModules.py
@@ -17,13 +17,6 @@ from sklearn import metrics
 
 class Sdc:
 
-    class DataGenerator():
-        def __init__(self):
-            self.xx = "hamid"
-
-    def __init__(self):
-        self.text = ""
-
     @staticmethod
     def import_simulated_images(data_path, csv_file, limit=0, augment_data=False):
         csv_lines = []
@@ -73,6 +66,61 @@ class Sdc:
                     break
 
         return (np.asarray(center_images), np.asarray(steering_angles))
+
+    @staticmethod
+    def generate_partition_ids(data_path, csv_file, limit=0, augment_data=False, validation_split=0.2):
+
+        csv_lines = []
+
+        with open(csv_file) as csv_file:
+            reader = csv.reader(csv_file)
+
+            for csv_line in reader:
+                csv_lines.append(csv_line)
+
+        partitions = {"train": [], "validation": []}
+
+        center_images = []
+        left_images = []
+        right_images = []
+        steering_angles = []
+        center_image_files = []
+        exception_count = 0
+
+        limit_counter = 0
+
+        for line_segments in csv_lines:
+            # print(line_segments)
+
+            try:
+                steering_angle = float(line_segments[3])
+                steering_angles.append(steering_angle)
+
+                image_file = data_path + line_segments[0]
+                center_image_files.append(image_file)
+                # use this line in case this code runs on the server
+                # (center_image_path, center_image_filename) = os.path.split(center_image_file)
+
+                image = cv2.imread(image_file)
+                center_images.append(image)
+
+                if (augment_data):
+                    steering_angles.append(steering_angle * -1.0)
+                    center_images.append(cv2.flip(image, 1))
+
+            except ValueError:
+                if (exception_count > 1):
+                    raise Exception("The CSV file is likely corrupted")
+
+                exception_count += 1
+
+            if (limit != 0):
+                limit_counter += 1
+
+                if (limit_counter > limit):
+                    break
+
+        return []
 
     @staticmethod
     def dummy():
